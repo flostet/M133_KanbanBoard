f415eda787a68ebec508c30b80f08d79651e12cf
Florian <florian_stettler@sluz.ch>
2020-12-03 20:38:51 +0100

    added API Requests
    added some basic API Requests to webserver.js and updated the ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # M133 KanbanBoard
-Teamprojekt im Modul 133 in dem wir ein Knaben Board gemacht haben.
+Teamprojekt von Florian Stettler und Max Pfäffli im Modul 133 in dem wir ein Kanban Board gemacht haben.
 
 In der Root Folder des Projekts muss dieser Befehl ausgeführt werden um den webserver zu starten: deno run --allow-net --allow-read webserver.js 
 Die Webseite ist dann unter http://localhost:8000 verfügbar.
--- webserver.js
+++ webserver.js
@@ -1,8 +1,44 @@
 import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts"
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 const app = new Application()
 const router = new Router()
 
-//API Request kommt hierhin
+let cards = [];
+
+const columns = [
+  { id: 0, title: 'ToDo' },
+  { id: 1, title: 'in Progress' },
+  { id: 2, title: 'Done' },
+];
+
+
+router
+    .get('/columns', (context) => (context.response.body = columns))
+    .get("/cards", context => context.response.body = cards)
+    .get("/cards/:id", context => {
+        const index = cards.findIndex(c => c.id == context.params.id);
+        context.response.body = cards[index]
+        
+    })
+    .post("/cards", async context => {
+        const card = await context.request.body({ type: "json" }).value;
+        card.id = v4.generate();
+        cards = [
+            ...cards,
+            card
+        ];
+        context.response.body = card;
+    })
+    .delete("/cards/:id", context => {
+        cards = cards.filter(c => c.id != context.params.id);
+    })
+    .put("/cards/:id", async context => {
+        const card = await context.request.body({ type: "json" }).value;
+        const index = cards.findIndex(c => c.id == context.params.id);
+        card.id = cards[index].id;
+        cards[index] = card;
+        context.response.body = cards[index];
+    });
 
 app.use(router.routes());
 app.use(async context => {
